b89c7dc336b3a5dc548137a6fe601c2816d20f85
NihonjinMachan <naqeeb458@gmail.com>
2018-10-19 17:07:31 +0400

    corrected port for heroku

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 var app = express();
 
+var port = process.env.PORT || 8080;
+
 app.use(express.static('public'));
 
 app.get('/', (req, res)=>{
     res.sendfile('public/index.html');
 });
 
-app.listen(3000);
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
